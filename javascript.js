1db208dd7c260abd5ff1f242878f900ffcd78312
Tamada <155598617+huseintamada@users.noreply.github.com>
2025-01-12 02:21:52 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,61 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function () {
+        menu.toggle();
+    });
+    menu.click(function () {
+        menu.toggle();
+    });
+}
+// Pilih elemen video
+const video = document.getElementById('backgroundVideo');
+
+// Tambahkan event listener untuk scroll
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 0) {
+        // Tambahkan kelas 'hidden' saat halaman di-scroll
+        video.classList.add('hidden');
+    } else {
+        // Hapus kelas 'hidden' jika kembali ke atas
+        video.classList.remove('hidden');
+    }
+});
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) {
+        klikMenu();
+    }
+})
+
+//check lebar
+$(window).resize(function () {
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display", "block");
+        //display:block
+    } else {
+        menu.css("display", "none");
+    }
+    klikMenu();
+});
+
+//efek scroll 
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $(document).scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 0) {
+            $("nav").addClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+        } else {
+            $("nav").removeClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+        }
+    })
+});
